b658fbc6196dc542de08f9f20f61b56039d095f5
Harrison Hecker <harrison.hecker22@gmail.com>
2025-09-26 18:49:58 -0400

    ollama plan

--- app.py
+++ app.py
@@ -21,7 +21,11 @@ def test():
             messages[data] = "User"
 
             # TODO: research how to call ollama API, call it here
-            
+            # 1. build dictionary of request data (make sure stream parameter set to false)
+            # 2. establish link that we want to send request to (http://localhost:11434/api/generate)
+            # 3. Use ApiRequest class to send the request and get the response
+            # 4. Set the response to ollamaResponse variable
+
             ollamaResponse = ""
             messages[ollamaResponse] = "System"
         return render_template("index.html", messages=messages)
