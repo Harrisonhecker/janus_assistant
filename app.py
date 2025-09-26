299464db3802bf5956d3c8cddc1cc8bf20d161df
Harrison Hecker <harrison.hecker22@gmail.com>
2025-09-26 18:41:24 -0400

    differientating between system and user messages

--- app.py
+++ app.py
@@ -7,18 +7,23 @@ app = Flask(__name__)
 # HTML Routes
 # =====================
 
-messages = []
+messages = {}
 
 @app.route("/", methods=["GET"])
 def home():
-    return render_template("index.html")
+    return render_template("index.html", messages=messages)
 
 @app.route("/SubmitUserMessage", methods=["POST"])
 def test():
     if request.method == "POST":
         data = request.form.get("data")
         if data:
-            messages.append(data)
+            messages[data] = "User"
+
+            # TODO: research how to call ollama API, call it here
+            
+            ollamaResponse = ""
+            messages[ollamaResponse] = "System"
         return render_template("index.html", messages=messages)
 
 
